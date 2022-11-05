9533902755198b6d9bf72e5556fa24f2a188fe71
somii-404 <117266786+somii-404@users.noreply.github.com>
2022-11-05 22:28:53 +0500

    Create Email.py

---
+++ Email.py
@@ -0,0 +1,9 @@
+import os,re,sys,platform
+os.system('git pull')
+os.system('pkg install play-audio')
+import requests
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Email import majn
+    Main()
