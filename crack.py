5fd7326603987eba2fa213a5a8fe54febbbda001
somii-404 <117266786+somii-404@users.noreply.github.com>
2023-01-17 13:35:30 +0500

    Delete crack.py

--- crack.py
+++
@@ -1,9 +0,0 @@
-import os,re,sys,platform
-os.system('git pull')
-os.system('pkg install play-audio')
-import requests
-
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from crack import majn
-    Main()
